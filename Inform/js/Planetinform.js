4b18da5841068c581ab7f444742cfb655a103334
Glitch (solar-system-aframe) <none>
2020-09-05 19:14:58 +0000

    🍋🏟 Updated with Glitch

---
+++ Inform/js/Planetinform.js
@@ -0,0 +1,99 @@
+AFRAME.registerComponent("planetinfo", {
+  //places planetinfo on planet so I can change info
+  schema: {
+    width: { type: "number", default: 1 },
+    height: { type: "number", default: 1.3 },
+    color: { type: "color", default: "#145378" },
+    info: { type: "string", default: "" },
+    infocolor: { type: "color", default: "#0de01f" },
+    infoposition: { type: "vec3" }
+  },
+
+  init: function() {
+    var el = this.el;
+    let x = document.querySelector("#x");
+    let data = this.data;
+    var geometry = new THREE.PlaneBufferGeometry(data.width, data.height);
+    var a = new THREE.Vector3(0.25, 0.25, 0.25);
+    var player2 = document.querySelector("#player2");
+    var planet = document.querySelector(".planet");
+    planet.play();
+    var material = new THREE.MeshStandardMaterial({
+      color: data.color,
+      side: THREE.DoubleSide
+    });
+
+    function createInfo() {
+      var player2 = document.querySelector("#player2");
+      player2.setAttribute("camera", "active", true);
+      var info = document.createElement("a-entity");
+
+      info.id = "info";
+      info.setAttribute("class", "planetInfo");
+      info.setObject3D("mesh", new THREE.Mesh(geometry, material));
+
+      info.setAttribute("position", data.infoposition);
+      info.setAttribute("text", {
+        value: data.info,
+        font: "https://cdn.aframe.io/fonts/SourceCodePro.fnt",
+        color: data.infocolor
+      });
+      player2.appendChild(info);
+    }
+    el.addEventListener("click", function() {
+      createInfo();
+      planet.pause();
+      x.object3D.visible = true;
+    });
+
+    el.addEventListener("mouseenter", function(event) {
+     
+      planet.pause();
+      el.object3D.scale.add(a);
+    });
+    el.addEventListener("mouseleave", function(back) {
+      planet.play();
+      el.object3D.scale.sub(a);
+    });
+  },
+
+  update: function(oldData) {
+    var data = this.data;
+    var el = this.el;
+    var info = document.querySelector("#info");
+
+    if (Object.keys(oldData).length === 0) {
+      return;
+    }
+    if (
+      data.width !== oldData.width ||
+      data.height !== oldData.height ||
+      data.color !== oldData.color
+    ) {
+      document
+        .getElementById("#info")
+        .getObject3D("mesh").geometry = new THREE.PlaneBufferGeometry(
+        data.width,
+        data.height
+      );
+      document
+        .getElementById("#info")
+        .getObject3D("mesh").material = new THREE.MeshStandardMaterial({
+        color: data.color,
+        side: THREE.DoubleSide
+      });
+    }
+    if (
+      data.info !== oldData.info ||
+      data.infocolor !== oldData.infocolor ||
+      data.infoposition !== oldData.infoposition
+    ) {
+      document.getElementById("#info").setAttribute("text", {
+        value: data.info,
+        font: "https://cdn.aframe.io/fonts/SourceCodePro.fnt",
+        color: data.infocolor
+      });
+      document.getElementById("#info").object3D.position.set(data.infoposition);
+    }
+  }
+});
---
+++ Inform/js/xbutton.js
@@ -0,0 +1,86 @@
+AFRAME.registerComponent("exitinfo", {
+  init: function() {
+    var el = this.el;
+    var planets = [
+      document.querySelector("#earth"),
+      document.querySelector("#venus"),
+      document.querySelector("#sun"),
+      document.querySelector("#mercury"),
+      document.querySelector("#mars"),
+      document.querySelector("#saturn"),
+      document.querySelector("#jupiter"),
+      document.querySelector("#uranus"),
+      document.querySelector("#neptune")
+    ];
+
+    var xpos;
+    function changeXpos(xpos) {
+      switch (xpos) {
+        case "Earth":
+          el.object3D.position.set(0.23, 0.34, -0.5);
+          break;
+        case "Venus":
+          el.object3D.position.set(0.209, 0.37, -0.5);
+          break;
+        case "Mars":
+          el.object3D.position.set(0.21, 0.4, -0.5);
+          break;
+        case "Mercury":
+          el.object3D.position.set(0.209, 0.37, -0.5);
+          break;
+        case "Jupiter":
+          el.object3D.position.set(0.21, 0.39, -0.5);
+          break;
+        case "Saturn":
+          el.object3D.position.set(0.2, 0.39, -0.5);
+      break; 
+        case "Uranus": 
+          el.object3D.position.set(0.199,0.39,-0.5)
+      break;
+        case "Neptune": 
+          el.object3D.position.set(0.18,0.39,-0.5); 
+          break;
+      }
+    }
+
+    planets[0].addEventListener("click", function() {
+      var xpos = "Earth";
+      changeXpos(xpos);
+    });
+    planets[1].addEventListener("click", function() {
+      var xpos = "Venus";
+      changeXpos(xpos);
+    });
+    planets[4].addEventListener("click", function() {
+      var xpos = "Mars";
+      changeXpos(xpos);
+    });
+    planets[3].addEventListener("click", function() {
+      var xpos = "Mercury";
+      changeXpos(xpos);
+    });
+    planets[6].addEventListener("click", function() {
+      var xpos = "Jupiter";
+      changeXpos(xpos);
+    });
+    planets[5].addEventListener("click", function() {
+      var xpos = "Saturn";
+      changeXpos(xpos);
+    });
+ planets[7].addEventListener("click", function() {
+      var xpos = "Uranus";
+      changeXpos(xpos);
+    }); 
+ planets[8].addEventListener("click",function(){ 
+ var xpos = "Neptune" 
+ changeXpos(xpos)
+ })
+    el.addEventListener("click", function exit(info) {
+      var info = document.querySelector("#info");
+
+      info.parentNode.removeChild(info);
+      el.object3D.visible = false;
+      document.querySelector("#player").setAttribute("camera", "active", true);
+    });
+  }
+});
